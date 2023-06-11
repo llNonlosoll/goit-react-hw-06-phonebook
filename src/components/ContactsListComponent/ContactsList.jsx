78a30f115590677d7c38a3a5f33ae328a6a2503f
llNonlosoll <wishmasterar@gmail.com>
2023-06-11 19:41:51 +0200

    contactsList done

--- src/components/ContactsListComponent/ContactsList.jsx
+++ src/components/ContactsListComponent/ContactsList.jsx
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types';
 import {
   ContactsListList,
   ContactsListItem,
@@ -7,36 +6,28 @@ import {
 } from './ContactsList.styled';
 
 // Список контактів
-export const Contacts = ({ contactsList, onContactRemove }) => (
-  <ContactsListList>
-    {contactsList.map(contact => (
-      <ContactsListItem key={contact.id}>
-        <ContactsListItemContainer>
-          <p>{contact.name} :</p>
-          <p>{contact.number}</p>
-        </ContactsListItemContainer>
+export function Contacts({ contactsList, onContactRemove }) {
+  return (
+    <ContactsListList>
+      {contactsList.map(contact => (
+        <ContactsListItem key={contact.id}>
+          <ContactsListItemContainer>
+            <p>{contact.name} :</p>
+            <p>{contact.number}</p>
+          </ContactsListItemContainer>
 
-        {
-          // Кнопка видалення контакту
-          <ContactsListButton
-            type="button"
-            name="delete"
-            onClick={() => onContactRemove(contact.id)}
-          >
-            Delete
-          </ContactsListButton>
-        }
-      </ContactsListItem>
-    ))}
-  </ContactsListList>
-);
-
-Contacts.propTypes = {
-  contactsList: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ),
-  onContactRemove: PropTypes.func.isRequired,
-};
+          {
+            // Кнопка видалення контакту
+            <ContactsListButton
+              type="button"
+              name="delete"
+              onClick={() => onContactRemove(contact.id)}
+            >
+              Delete
+            </ContactsListButton>
+          }
+        </ContactsListItem>
+      ))}
+    </ContactsListList>
+  );
+}
